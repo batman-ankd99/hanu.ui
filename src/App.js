a398d3e13fe11aef5cb59bea16ebf7b1b54c6a18
ankur <ankd99@gmail.com>
2025-11-22 22:53:58 +0530

    removed ec2 and s3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import EC2 from "./pages/EC2";
 import SG from "./pages/SG";
-import S3 from "./pages/S3";
 import IAM from "./pages/IAM";
 
 function App() {
@@ -9,16 +7,12 @@ function App() {
     <Router>
       <div className="p-4">
         <nav className="flex gap-4 bg-gray-100 p-3 rounded">
-          <Link to="/ec2" className="text-blue-600">EC2</Link>
           <Link to="/sg" className="text-blue-600">Security Groups</Link>
-          <Link to="/s3" className="text-blue-600">S3</Link>
           <Link to="/iam" className="text-blue-600">IAM</Link>
         </nav>
 
         <Routes>
-          <Route path="/ec2" element={<EC2 />} />
           <Route path="/sg" element={<SG />} />
-          <Route path="/s3" element={<S3 />} />
           <Route path="/iam" element={<IAM />} />
         </Routes>
       </div>
