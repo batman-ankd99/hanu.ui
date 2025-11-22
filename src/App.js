54de32109c217c1cfa5197a487fcfc45e86235bb
ankur <ankd99@gmail.com>
2025-11-23 03:40:09 +0530

    iam keys ui [page

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ function App() {
         <nav className="flex gap-4 bg-gray-100 p-3 rounded">
           <Link to="/sg" className="text-blue-600">Security Groups</Link>
           <Link to="/iam" className="text-blue-600">IAM</Link>
-          <Link to="/iam" className="text-blue-600">IAM_USERACCESSKEY</Link>
+          <Link to="/iam_useraccesskey" className="text-blue-600">IAM_USERACCESSKEY</Link>
         </nav>
 
         <Routes>
           <Route path="/sg" element={<SG />} /> #element is a component point to SG.js
           <Route path="/iam" element={<IAM />} />
-          <Route path="/iam" element={<IAM_USERACCESSKEY />} />
+          <Route path="/iam_useraccesskey" element={<IAM_USERACCESSKEY />} />
         </Routes>
       </div>
     </Router>
