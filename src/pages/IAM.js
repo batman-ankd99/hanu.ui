696b75554dffff5bbcca480362dc448df693282d
ankur <ankd99@gmail.com>
2025-11-22 20:59:05 +0530

    changes to fix

---
+++ src/pages/IAM.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+
+export default function IAM() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/analyzer/iam")
+      .then(res => res.json())
+      .then(data => setData(data));
+  }, []);
+
+  if (!data) return <p>Loading...</p>;
+
+  return (
+    <div className="mt-4">
+      <h1 className="text-2xl font-bold">IAM Policy Audit</h1>
+      <pre className="bg-black text-green-400 p-4 rounded mt-2">
+        {JSON.stringify(data, null, 2)}
+      </pre>
+    </div>
+  );
+}
---
+++ src/pages/S3.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+
+export default function S3() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/analyzer/s3")
+      .then(res => res.json())
+      .then(data => setData(data));
+  }, []);
+
+  if (!data) return <p>Loading...</p>;
+
+  return (
+    <div className="mt-4">
+      <h1 className="text-2xl font-bold">S3 Bucket Risks</h1>
+      <pre className="bg-black text-green-400 p-4 rounded mt-2">
+        {JSON.stringify(data, null, 2)}
+      </pre>
+    </div>
+  );
+}
