adcbcd2121012a3dfd44f7993adc66b78cafe60e
ankur <ankd99@gmail.com>
2026-05-01 00:15:24 +0530

    removed iam tab

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import SG from "./pages/SG";
 import IAM from "./pages/IAM";
 import IAM_USERACCESSKEY from "./pages/IAM_USERACCESSKEY";
@@ -7,21 +7,18 @@ import Dashboard from "./pages/Dashboard";
 function App() {
   return (
     <Router>
-      <div className="p-4">
-        <nav className="flex gap-4 bg-gray-100 p-3 rounded">
-          <Link to="/sg" className="text-blue-600">Security Groups</Link>
-          <Link to="/iam" className="text-blue-600">IAM</Link>
-          <Link to="/iam_useraccesskey" className="text-blue-600">IAM_USERACCESSKEY</Link>
-          <Link to="/dashboard" className="text-blue-600">Dashboard</Link>
-        </nav>
+      <Routes>
 
-        <Routes>
-          <Route path="/sg" element={<SG />} /> #element is a component point to SG.js
-          <Route path="/iam" element={<IAM />} />
-          <Route path="/iam_useraccesskey" element={<IAM_USERACCESSKEY />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </div>
+        {/* Default route → Dashboard */}
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+
+        {/* Pages */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sg" element={<SG />} />
+        <Route path="/iam" element={<IAM />} />
+        <Route path="/iam_useraccesskey" element={<IAM_USERACCESSKEY />} />
+
+      </Routes>
     </Router>
   );
 }
